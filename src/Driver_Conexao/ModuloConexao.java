f4587caec14bce2e2b97cc5cfdeaaaeaadbd6548
Samuel-art754 <67794795+Samuel-art754@users.noreply.github.com>
2020-07-03 15:23:17 -0300

    Commit Tela de login do usuario e driver de conexão.

---
+++ src/Driver_Conexao/ModuloConexao.java
@@ -0,0 +1,56 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Driver_Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ModuloConexao {
+     
+    //private String hostname;
+    //private int port;
+    //private String database;
+    //private String username;
+    //private String password;
+    
+    //private Connection conexao;
+    
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        
+        // CHAMAR O DRIVER
+        
+        String driver = "com.mysql.jdbc.Driver";
+        
+        // INFO´S DO BANCO
+        
+        String url = "jdbc:mysql://localhost:3306/corporativaBD?useTimezone=true&serverTimezone=UTC";
+        String user = "root";
+        String password = "";
+        
+        // ESTABELECENDO CONEXAO COM O BANCO
+    
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            DriverManager.registerDriver (new com.mysql.jdbc.Driver());
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        }
+        catch(SQLException e){
+            JOptionPane.showMessageDialog(null, e+ " Conexão não estabelecida");
+            return null;
+            }
+        
+    }
+}
+   
